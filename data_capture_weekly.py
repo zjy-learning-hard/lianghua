76c7e5da9796174e697368e1caa8c0379555c31d
zjy-learning-hard <87358104+zjy-learning-hard@users.noreply.github.com>
2025-02-14 17:12:30 +0800

    Add files via upload

--- data_capture_weekly.py
+++ data_capture_weekly.py
@@ -9,6 +9,7 @@ import time
 import akshare as ak
 from datetime import datetime, timedelta
 
+
 def get_date(days_diff=0):
     """获取与当前日期相差指定天数的日期
     :param days_diff: 天数差（正数为未来，负数为过去）
@@ -18,18 +19,44 @@ def get_date(days_diff=0):
     return target_date.strftime("%Y%m%d")
 
 
-def get_3second_data_weekly():
+
+if __name__ == "__main__":
+    today = datetime.today()
+    four_days_ago = today - timedelta(days=4)
+    end_dt = today.strftime("%Y-%m-%d")+' 15:00:00'
+    start_dt = four_days_ago.strftime("%Y-%m-%d")+' 09:30:00'
+    '''伊利'''    
     for i in range(5):
         data_dt = get_date(i)
         print(f"执行任务日期: {data_dt}")
-        print("正在获取数据")
-        stock_intraday_sina_df = ak.stock_intraday_sina(symbol="sh600751", date=data_dt)
-        file_name = 'sh600751_'+data_dt+'.csv'
+        print("正在获取伊利交易数据")
+        stock_intraday_sina_df = ak.stock_intraday_sina(symbol="sh600887", date=data_dt)
+        file_name = r"C:\Users\17317\Desktop\lianghua\yili\trade_data\sh600887_trade"+data_dt+'.csv'
         stock_intraday_sina_df.to_csv(file_name, index=False)
         print("CSV 文件保存成功！")
         time.sleep(60)
         
+    stock_zh_a_hist_min_em_df = ak.stock_zh_a_hist_min_em(symbol="600887", start_date=start_dt, end_date=end_dt, period="1", adjust="")        
+    stock_zh_a_hist_min_em_df.to_csv(r"C:\Users\17317\Desktop\lianghua\yili\minute_data\sh600887_"+today.strftime("%Y-%m-%d")+".csv", index=False)
+    
+    '''茅台'''      
+    for i in range(5):
+        data_dt = get_date(i)
+        print(f"执行任务日期: {data_dt}")
+        print("正在获取茅台交易数据")
+        stock_intraday_sina_df = ak.stock_intraday_sina(symbol="sh600519", date=data_dt)
+        file_name = r"C:\Users\17317\Desktop\lianghua\maotai\trade_data\sh600519_trade"+data_dt+'.csv'
+        stock_intraday_sina_df.to_csv(file_name, index=False)
+        print("CSV 文件保存成功！")
+        time.sleep(60)
+    stock_zh_a_hist_min_em_df = ak.stock_zh_a_hist_min_em(symbol="600519", start_date=start_dt, end_date=end_dt, period="1", adjust="")        
+    stock_zh_a_hist_min_em_df.to_csv(r"C:\Users\17317\Desktop\lianghua\yili\minute_data\sh600519_"+today.strftime("%Y-%m-%d")+".csv", index=False)
+              
+        
+        '''纳斯达克'''
+    fund_etf_hist_min_em_df = ak.fund_etf_hist_min_em(symbol="513300", period="1", adjust="", start_date=start_dt, end_date=end_dt)
+    fund_etf_hist_min_em_df.to_csv(r"C:\Users\17317\Desktop\lianghua\nasidake\minute_data\nasidake_"+today.strftime("%Y-%m-%d")+".csv", index=False)
 
-
-
-
+        '''黄金etf'''
+    fund_etf_hist_min_em_df = ak.fund_etf_hist_min_em(symbol="518880", period="1", adjust="", start_date=start_dt, end_date=end_dt)
+    fund_etf_hist_min_em_df.to_csv(r"C:\Users\17317\Desktop\lianghua\gold\minute_data\gold_"+today.strftime("%Y-%m-%d")+".csv", index=False)
